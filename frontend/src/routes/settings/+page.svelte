eebd39cee1954ef56dcb92edcd84b4a0ba981db9
sumeet <Sumeet.Mathpati@steel-eye.com>
2026-02-14 21:56:00 +0530

    show settings

---
+++ frontend/src/routes/settings/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import * as Tabs from '$lib/components/ui/tabs/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+</script>
+
+<div class="h-screen overflow-y-auto p-4">
+	<Tabs.Root value="account">
+		<Tabs.List>
+			<Tabs.Trigger value="apps">Apps</Tabs.Trigger>
+			<Tabs.Trigger value="settings">Settings</Tabs.Trigger>
+		</Tabs.List>
+		<Tabs.Content value="apps">Make changes to your apps here.</Tabs.Content>
+		<Tabs.Content value="settings">Change your settings here.</Tabs.Content>
+	</Tabs.Root>
+</div>
